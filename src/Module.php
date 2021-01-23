5c9959dcb584100e596adc712d5dc548308591c9
Rico <mail@ricosonntag.de>
2021-01-23 14:24:19 +0100

    Fixed module version

--- src/Module.php
+++ src/Module.php
@@ -55,7 +55,7 @@ class Module extends PedigreeChartModule implements ModuleCustomInterface
     /**
      * @var string
      */
-    public const CUSTOM_VERSION = '1.1.0';
+    public const CUSTOM_VERSION = '1.2.1';
 
     /**
      * @var string
