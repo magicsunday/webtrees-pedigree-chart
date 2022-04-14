870b14a94cb329eb08d60b70329c83ba7482de47
Rico Sonntag <mail@ricosonntag.de>
2022-04-14 11:53:32 +0200

    Fixed default parameter

--- resources/js/modules/common/dataUrl.js
+++ resources/js/modules/common/dataUrl.js
@@ -5,22 +5,22 @@
 /**
  * Returns the input as data URL, prefixed with data: scheme.
  *
- * @param {RequestInfo} input
- * @param {RequestInit} init
+ * @param {RequestInfo}      input
+ * @param {null|RequestInit} init
  *
  * @returns {Promise<unknown>}
  */
-export default function(input, init)
+export default function(input, init = null)
 {
     return fetch(input, init)
         .then(response => response.blob())
         .then(blob => new Promise(
-            (resolve, reject) => {
-                const reader = new FileReader();
-                reader.onloadend = () => resolve(reader.result);
-                reader.onerror = reject;
-                reader.readAsDataURL(blob);
-            }
-        )
-    );
+                (resolve, reject) => {
+                    const reader = new FileReader();
+                    reader.onloadend = () => resolve(reader.result);
+                    reader.onerror = reject;
+                    reader.readAsDataURL(blob);
+                }
+            )
+        );
 }
