24e62afc1cd2c5a7d0016d9256e2290d16238c42
Rico Sonntag <mail@ricosonntag.de>
2023-01-26 12:06:07 +0100

    1.5.1-dev

--- src/Module.php
+++ src/Module.php
@@ -59,7 +59,7 @@ class Module extends PedigreeChartModule implements ModuleCustomInterface
     /**
      * @var string
      */
-    public const CUSTOM_VERSION = '1.5.0';
+    public const CUSTOM_VERSION = '1.5.1-dev';
 
     /**
      * @var string
