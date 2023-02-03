075dbaf56eeb4ea4778003ccffaab126f5ec2cb9
Rico Sonntag <mail@ricosonntag.de>
2023-02-03 15:01:22 +0100

    Remove duplicate code to detect module version

--- src/Traits/ModuleCustomTrait.php
+++ src/Traits/ModuleCustomTrait.php
@@ -11,11 +11,8 @@ declare(strict_types=1);
 
 namespace MagicSunday\Webtrees\PedigreeChart\Traits;
 
-use Fig\Http\Message\StatusCodeInterface;
 use Fisharebest\Localization\Translation;
-use Fisharebest\Webtrees\Registry;
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\GuzzleException;
+use MagicSunday\Webtrees\ModuleBase\Module\VersionInformation;
 
 /**
  * Trait ModuleCustomTrait.
@@ -43,49 +40,14 @@ trait ModuleCustomTrait
         return self::CUSTOM_LATEST_VERSION;
     }
 
+//    protected function extractVersion(string $content): string
+//    {
+//        return json_decode($content, true)['tag_name'] ?? '';
+//    }
+
     public function customModuleLatestVersion(): string
     {
-        // No update URL provided.
-        if ($this->customModuleLatestVersionUrl() === '') {
-            return $this->customModuleVersion();
-        }
-
-        return Registry::cache()->file()->remember(
-            $this->name() . '-latest-version',
-            function (): string {
-                try {
-                    $client = new Client([
-                        'timeout' => 3,
-                    ]);
-
-                    $response = $client->get($this->customModuleLatestVersionUrl());
-
-                    if ($response->getStatusCode() === StatusCodeInterface::STATUS_OK) {
-                        $json = json_decode(
-                            $response->getBody()->getContents(),
-                            true,
-                            512,
-                            JSON_THROW_ON_ERROR
-                        );
-
-                        if (is_array($json)) {
-                            /** @var string $version */
-                            $version = $json['tag_name'] ?? '';
-
-                            // Does the response look like a version?
-                            if (preg_match('/^\d+\.\d+\.\d+/', $version) === 1) {
-                                return $version;
-                            }
-                        }
-                    }
-                } catch (GuzzleException $exception) {
-                    // Can't connect to the server?
-                }
-
-                return $this->customModuleVersion();
-            },
-            86400
-        );
+        return (new VersionInformation($this))->fetchLatestVersion();
     }
 
     public function customModuleSupportUrl(): string
