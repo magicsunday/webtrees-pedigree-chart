b34d7c5f53c2f01ad2c97412053d8ba9f5ddf54a
Rico Sonntag <mail@ricosonntag.de>
2023-08-31 11:08:34 +0200

    Increase slightly the size of the individual box

--- resources/js/modules/constants.js
+++ resources/js/modules/constants.js
@@ -10,7 +10,7 @@
  *
  * @type {Number}
  */
-export const LAYOUT_HORIZONTAL_NODE_WIDTH  = 300;
+export const LAYOUT_HORIZONTAL_NODE_WIDTH  = 325;
 export const LAYOUT_HORIZONTAL_NODE_HEIGHT = 80;
 export const LAYOUT_VERTICAL_NODE_WIDTH    = 150;
 export const LAYOUT_VERTICAL_NODE_HEIGHT   = 175;
