89ad46481306b2b8daaaa9ef82fb1334a38ed21b
Rico Sonntag <mail@ricosonntag.de>
2024-03-17 20:56:08 +0100

    Fixes #67: Chinese surname and first name are in the wrong position

--- resources/js/modules/lib/tree/name.js
+++ resources/js/modules/lib/tree/name.js
@@ -10,6 +10,7 @@ import OrientationTopBottom from "../chart/orientation/orientation-topBottom";
 import OrientationBottomTop from "../chart/orientation/orientation-bottomTop";
 import OrientationLeftRight from "../chart/orientation/orientation-leftRight";
 import OrientationRightLeft from "../chart/orientation/orientation-rightLeft";
+import * as d3 from "../d3.js";
 
 /**
  * The class handles the creation of the tree.
@@ -53,43 +54,42 @@ export default class Name
         if ((this._orientation instanceof OrientationTopBottom)
             || (this._orientation instanceof OrientationBottomTop)
         ) {
+            const that = this;
+
             const enter = name.selectAll("text")
                 .data(datum => [
                     {
                         data: datum.data,
                         isRtl: datum.data.data.isNameRtl,
                         isAltRtl: datum.data.data.isAltRtl,
+                        // Always arrange the text at the same position regardless if an image is displayed or not
                         withImage: true
                     }
                 ])
                 .enter();
 
             enter
-                .call((g) => {
-                    const text = g.append("text")
-                        .attr("class", "wt-chart-box-name")
-                        .attr("text-anchor", "middle")
-                        .attr("direction", d => d.isRtl ? "rtl" : "ltr")
-                        .attr("alignment-baseline", "central")
-                        .attr("y", this._text.y - 5);
+                .each(function (d) {
+                    const element = d3.select(this);
+                    const nameGroups = that.createNamesData(d);
 
-                    this.addNameElements(
-                        text,
-                        datum => this.createNamesData(text, datum, true, false)
-                    );
-                })
-                .call((g) => {
-                    const text = g.append("text")
-                        .attr("class", "wt-chart-box-name")
-                        .attr("text-anchor", "middle")
-                        .attr("direction", d => d.isRtl ? "rtl" : "ltr")
-                        .attr("alignment-baseline", "central")
-                        .attr("y", this._text.y + 15);
+                    nameGroups.forEach((nameGroup, index) => {
+                        const text = element.append("text")
+                            .attr("class", "wt-chart-box-name")
+                            .attr("text-anchor", "middle")
+                            .attr("direction", d => d.isRtl ? "rtl" : "ltr")
+                            .attr("alignment-baseline", "central")
+                            .attr("y", that._text.y - 5 + (index * 20));
 
-                    this.addNameElements(
-                        text,
-                        datum => this.createNamesData(text, datum, false, true)
-                    );
+                        that.addNameElements(
+                            text,
+                            that.truncateNamesData(
+                                text,
+                                nameGroup,
+                                d.withImage
+                            )
+                        );
+                    });
                 });
 
             // Add alternative name if present
@@ -144,7 +144,20 @@ export default class Name
 
                     this.addNameElements(
                         text,
-                        datum => this.createNamesData(text, datum, true, true)
+                        (datum) => {
+                            const nameGroups = this.createNamesData(datum);
+
+                            return this.truncateNamesData(
+                                text,
+                                // Merge the firstname and lastname groups together,
+                                // as we display the whole name in one line
+                                [
+                                    ...nameGroups[0],
+                                    ...nameGroups[1],
+                                ],
+                                datum.withImage
+                            )
+                        }
                     );
                 });
 
@@ -207,67 +220,89 @@ export default class Name
     }
 
     /**
+     * Creates the data array for the names in top/bottom layout.
+     *
+     * @param {NameElementData} datum
+     *
+     * @return {LabelElementData[][]}
+     *
+     * @private
+     */
+    createNamesData(datum)
+    {
+        /** @var {LabelElementData[][]} names */
+        let names = {};
+        /** @var {LabelElementData[]} firstnames */
+        let firstnames = {};
+        /** @var {LabelElementData[]} lastnames */
+        let lastnames = {};
+        let minPosFirstnames = Number.MAX_SAFE_INTEGER;
+        let minPosLastnames = Number.MAX_SAFE_INTEGER;
+
+        // Iterate over the individual name components and determine their position in the overall
+        // name and insert the component at the corresponding position in the result object.
+        for (let i in datum.data.data.firstNames) {
+            const pos = datum.data.data.name.indexOf(datum.data.data.firstNames[i]);
+
+            if (pos !== -1) {
+                if (pos < minPosFirstnames) {
+                    minPosFirstnames = pos;
+                }
+
+                firstnames[pos] = {
+                    label: datum.data.data.firstNames[i],
+                    isPreferred: datum.data.data.firstNames[i] === datum.data.data.preferredName,
+                    isLastName: false,
+                    isNameRtl: datum.data.data.isNameRtl
+                };
+            }
+        }
+
+        names[minPosFirstnames] = Object.values(firstnames);
+
+        for (let i in datum.data.data.lastNames) {
+            const pos = datum.data.data.name.indexOf(datum.data.data.lastNames[i]);
+
+            if (pos !== -1) {
+                if (pos < minPosLastnames) {
+                    minPosLastnames = pos;
+                }
+
+                lastnames[pos] = {
+                    label: datum.data.data.lastNames[i],
+                    isPreferred: false,
+                    isLastName: true,
+                    isNameRtl: datum.data.data.isNameRtl
+                };
+            }
+        }
+
+        names[minPosLastnames] = Object.values(lastnames);
+
+        // Extract the values (keys doesn't matter anymore)
+        return Object.values(names);
+    }
+
+    /**
      * Creates the data array for the names.
      *
-     * @param {Object}          parent
-     * @param {NameElementData} datum
-     * @param {Boolean}         addFirstNames
-     * @param {Boolean}         addLastNames
+     * @param {Object}             parent
+     * @param {LabelElementData[]} names
+     * @param {Boolean}            withImage
      *
      * @return {LabelElementData[]}
      *
      * @private
      */
-    createNamesData(parent, datum, addFirstNames, addLastNames)
+    truncateNamesData(parent, names, withImage)
     {
-        /** @var {LabelElementData[]} names */
-        let names = [];
-
-        if (addFirstNames === true) {
-            names = names.concat(
-                datum.data.data.firstNames.map((firstName) => {
-                    return {
-                        label: firstName,
-                        isPreferred: firstName === datum.data.data.preferredName,
-                        isLastName: false,
-                        isNameRtl: datum.data.data.isNameRtl
-                    }
-                })
-            );
-        }
-
-        if (addLastNames === true) {
-            // Append the last names
-            names = names.concat(
-                datum.data.data.lastNames.map((lastName) => {
-                    return {
-                        label: lastName,
-                        isPreferred: false,
-                        isLastName: true,
-                        isNameRtl: datum.data.data.isNameRtl
-                    }
-                })
-            );
-        }
-
-        // // If both first and last names are empty, add the full name as an alternative
-        // if (!datum.data.data.firstNames.length
-        //     && !datum.data.data.lastNames.length
-        // ) {
-        //     names = names.concat([{
-        //         label: datum.data.data.name,
-        //         isPreferred: false,
-        //         isLastName: false
-        //     }]);
-        // }
-
         const fontSize   = parent.style("font-size");
         const fontWeight = parent.style("font-weight");
 
         // The total available width that the text can occupy
         let availableWidth = this._text.width;
 
-        if (datum.withImage) {
+        if (withImage) {
             if ((this._orientation instanceof OrientationLeftRight)
                 || (this._orientation instanceof OrientationRightLeft)
             ) {
@@ -358,7 +393,7 @@ export default class Name
                 return name;
             })
             .map((name) => {
-                // Afterward, the preferred ones if text takes still too much space
+                // Afterward, the preferred ones, if text takes still too much space
                 if (name.isPreferred === true) {
                     if (this.measureText(text, fontSize, fontWeight) > availableWidth) {
                         // Keep only the first letter
