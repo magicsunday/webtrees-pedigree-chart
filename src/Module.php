48a0b05dbf8bf2da410ddf192e3d6289de1acf29
Rico Sonntag <mail@ricosonntag.de>
2026-05-02 19:35:28 +0200

    Release 2.5.0

--- src/Module.php
+++ src/Module.php
@@ -64,7 +64,7 @@ class Module extends PedigreeChartModule implements ModuleAssetUrlInterface, Mod
     /**
      * @var string
      */
-    public const CUSTOM_VERSION = '2.4.1-dev';
+    public const CUSTOM_VERSION = '2.5.0';
 
     /**
      * @var string
