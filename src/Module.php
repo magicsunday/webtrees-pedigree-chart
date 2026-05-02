a1993180471cd88de5fe4ee8ea21d31ce25460bc
Rico Sonntag <mail@ricosonntag.de>
2026-05-02 16:10:56 +0200

    Bump version to 2.4.1-dev

--- src/Module.php
+++ src/Module.php
@@ -64,7 +64,7 @@ class Module extends PedigreeChartModule implements ModuleAssetUrlInterface, Mod
     /**
      * @var string
      */
-    public const CUSTOM_VERSION = '2.4.0';
+    public const CUSTOM_VERSION = '2.4.1-dev';
 
     /**
      * @var string
