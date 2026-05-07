29fe60cba0e6844bfb9cc97bf288d8b04e745955
Rico Sonntag <mail@ricosonntag.de>
2026-05-07 15:55:03 +0200

    Release 2.7.1

--- src/Module.php
+++ src/Module.php
@@ -64,7 +64,7 @@ class Module extends PedigreeChartModule implements ModuleAssetUrlInterface, Mod
     /**
      * @var string
      */
-    public const CUSTOM_VERSION = '2.7.1-dev';
+    public const CUSTOM_VERSION = '2.7.1';
 
     /**
      * Webtrees renders this URL as the "For more information, see …" link inside the
