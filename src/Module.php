13e726aab9a778c7a84cfb5dbbcdcac65dbfe9c1
Rico Sonntag <mail@ricosonntag.de>
2026-05-07 15:55:41 +0200

    Bump version to 2.7.2-dev

--- src/Module.php
+++ src/Module.php
@@ -64,7 +64,7 @@ class Module extends PedigreeChartModule implements ModuleAssetUrlInterface, Mod
     /**
      * @var string
      */
-    public const CUSTOM_VERSION = '2.7.1';
+    public const CUSTOM_VERSION = '2.7.2-dev';
 
     /**
      * Webtrees renders this URL as the "For more information, see …" link inside the
